5f8ee453ddc97e731191256d154757295a279713
dpws <dp@dpws.ru>
2026-05-21 18:05:41 +0100

    Input: re-export InputSize from barrel (Rule 5)

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -3,7 +3,7 @@ export { Button } from './components/Button';
 export type { ButtonProps, ButtonVariant, ButtonSize } from './components/Button';
 
 export { Input } from './components/Input';
-export type { InputProps } from './components/Input';
+export type { InputProps, InputSize } from './components/Input';
 
 export { Card } from './components/Card';
 export type { CardProps, CardPadding } from './components/Card';
